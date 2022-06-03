cc175683d201875b0d3d34a12f827c6b30cf626e
Olaniyi Raimot <olaniyi.adenike98@gmail.com>
2022-06-03 22:01:59 +0100

    randommnnumber

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if last_digit > 5:
     print(f"{last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"{last_digit}and is 0
-elif:
+else:
     print(f"{last_digit} and is less than 6 and not 0
